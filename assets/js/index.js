d37d931a96a5675e871bce43b97420730d478100
Sanskar Sahu <sahup3296@gmail.com>
2022-09-13 08:27:23 +0530

    ✔️ BOARD IS DONE

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,17 @@
-const canvas = document.getElementById("app");
+// all main variable and configrations.
+const canvas = document.getElementById("cvs");
 const ctx = canvas.getContext("2d");
-console.log(ctx);
+canvas.style.background = "black";
+const CH = (canvas.height = 400);
+const CW = (canvas.width = 400);
+
+canvas.addEventListener("mousemove", (e) => start(e));
+
+function start(e) {
+  ctx.fillStyle = "white";
+  ctx.beginPath();
+  ctx.arc(e.offsetX, e.offsetY, 10, 0, Math.PI * 2, true);
+  ctx.closePath();
+  ctx.fill();
+}
+start();
