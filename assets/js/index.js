1361b027bfa6bdae6bcc3f372c25b159da08f015
Sanskar Sahu <sahup3296@gmail.com>
2022-09-13 08:54:58 +0530

    update index.js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,30 @@
 // all main variable and configrations.
 const canvas = document.getElementById("cvs");
+const penColor = document.getElementById("sketchColor");
+const changeColor = document.getElementById("changeColor");
 const ctx = canvas.getContext("2d");
 canvas.style.background = "black";
 const CH = (canvas.height = 400);
 const CW = (canvas.width = 400);
+const sketchColor = penColor.value ? penColor : "white";
 
 canvas.addEventListener("mousemove", (e) => start(e));
+changeColor.addEventListener("onclick", getInputValue);
 
 function start(e) {
-  ctx.fillStyle = "white";
+  ctx.fillStyle = sketchColor;
   ctx.beginPath();
   ctx.arc(e.offsetX, e.offsetY, 10, 0, Math.PI * 2, true);
   ctx.closePath();
   ctx.fill();
 }
+
+function getInputValue() {
+  // Selecting the input element and get its value
+  let inputVal = penColor.value;
+
+  // Displaying the value
+  console.log(inputVal);
+}
+
 start();
