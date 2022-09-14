90fd86d440629ba5b9a16968de26290b1cf4c2eb
Sanskar Sahu <sahup3296@gmail.com>
2022-09-14 20:13:47 +0530

    WORKING ON

--- test.js
+++ test.js
@@ -6,15 +6,16 @@ const ColorPicker = {
 
     this.canvas.style.background = "black";
     this.ctx = this.canvas.getContext("2d");
-    this.gradient = this.ctx.createLinearGradient(0, 0, this.CW, 0);
     // Add three color stops
-    this.gradient.addColorStop(0, "green");
-    this.gradient.addColorStop(0.5, "cyan");
-    this.gradient.addColorStop(1, "green");
 
-    // Set the fill style and draw a rectangle
+    this.gradient = this.ctx.createLinearGradient(0, 0, this.CW, 0);
+    this.gradient.addColorStop(0.2, "red");
+    this.gradient.addColorStop(0.5, "black");
+    this.gradient.addColorStop(1, "white");
     this.ctx.fillStyle = this.gradient;
+
     this.ctx.fillRect(0, 0, this.CW, this.CW / 2);
+    // Set the fill style and draw a rectangle
   }
 };
 
