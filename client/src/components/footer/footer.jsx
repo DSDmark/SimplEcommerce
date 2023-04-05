14c9ac4da66d1475312ac85b4af4411de9abfc84
DSDmark <sahup3296@gmail.com>
2023-04-05 19:26:53 +0530

    update files

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Grid, Box, Typography, styled } from "@mui/material"
+import { shades } from '~/utils/theme';
 
 const Footer = () => {
+
+  const footerData = ["websiteLogo", "aboutus", "contact", "prievy"];
+  const FooterStyle = styled(Grid)(({ theme }) => ({
+    background: shades.pri[400],
+    padding: theme.spacing(5),
+    color: shades.sco[200],
+  }))
+
   return (
-    <div>Footerfooter</div>
+    <Box>
+      <Grid container mt={5} rowSpacing={2}>
+        {footerData.map((item) => {
+          return (
+            <FooterStyle itme>
+              <Typography variant="subtitle1">
+                {item}
+              </Typography>
+            </FooterStyle>
+          )
+        })}
+      </Grid>
+    </Box>
   )
 }
 
